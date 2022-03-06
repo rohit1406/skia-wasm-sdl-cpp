758ea7e104b5bfde6c36c4f876f1c91ecfff5b4e
Rohit M - Skia <rohit.muneshwar@ymail.com>
2022-03-06 19:04:22 +0100

    EMSCRIPTEN:: run c, cpp code in browser using emscripte. Read me file also updated with the steps

---
+++ src/hello_world.c
@@ -0,0 +1,11 @@
+/* Author:
+// Rohit Muneshwar: 06 March 2022
+// C hello world file  this will be used with emscripten to run on browser
+*/
+#include <stdio.h>
+
+int main() {
+	printf("Hello Emscripten!!!");
+	printf("\n");
+	return 0;
+}
---
+++ src/hello_world_sdl.cpp
@@ -0,0 +1,64 @@
+/* Author:
+// Rohit Muneshwar: 06 March 2022
+// This cpp file draws something on the screen using SDL and emscripten
+*/
+
+#include<stdio.h>
+#include<SDL/SDL.h>
+
+#ifdef __EMSCRIPTEN__
+#include<emscripten.h>
+#endif // !__EMSCRIPTEN__
+
+extern "C" int main(int args, char** argv) {
+	printf("Hello Emscripten!!!\n");
+
+	// Initialize SDL
+	printf("Initializing SDL\n");
+	SDL_Init(SDL_INIT_VIDEO);
+	SDL_Surface* screen = SDL_SetVideoMode(256, 256, 32, SDL_SWSURFACE);
+	printf("SDL Initialzed\n");
+	#ifdef TEST_SDL_LOCK_OPTS
+		EM_ASM("SDL.defaults.copyOnLock = false; SDL.defaults.discardOnLock = true; SDL.defaults.opaqueFrontBuffer = false;");
+	#endif
+
+	if (SDL_MUSTLOCK(screen))
+		SDL_LockSurface(screen);
+	printf("Drawing Square\n");
+	// Draw Square
+	for (int i = 0; i < 256; i++) {
+		for (int j = 0; j < 256; j++) {
+			#ifdef TEST_SDL_LOCK_OPTS
+						// Alpha behaves like in the browser, so write proper opaque pixels.
+						int alpha = 255;
+			#else
+						// To emulate native behavior with blitting to screen, alpha component is ignored. Test that it is so by outputting
+						// data (and testing that it does get discarded)
+						int alpha = (i + j) % 255;
+			#endif
+						* ((Uint32*)screen->pixels + i * 256 + j) = SDL_MapRGBA(screen->format, i, j, 255 - i, alpha);
+		}
+	}
+
+
+
+	if (SDL_MUSTLOCK(screen))
+		SDL_UnlockSurface(screen);
+
+	SDL_Flip(screen);
+
+	printf("you should see a smoothly-colored square - no sharp lines but the square borders!\n");
+	printf("and here is some text that should be HTML-friendly: amp: |&| double-quote: |\"| quote: |'| less-than, greater-than, html-like tags: |<cheez></cheez>|\nanother line.\n");
+
+
+	// Quit SDL
+	SDL_Quit();
+
+	// Close Log File
+	printf("returning from main\n");
+
+	// Flushes the output to the canvas - this is required when using emscripten
+	printf("\n");
+
+	return 0;
+}
